ba1e80970eb5a0eb6b828e9b7508068567220a59
ACER <ACER@LAPTOP-NND0A16U>
2024-11-26 22:19:37 +0700

    Add project files.

---
+++ BTVN_Buoi3/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace BTVN_Buoi3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvNhanVien = new System.Windows.Forms.ListView();
+            this.colLastName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colFirstName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colPhone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_addname = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_remove = new System.Windows.Forms.Button();
+            this.txt_phone = new System.Windows.Forms.TextBox();
+            this.txt_firstname = new System.Windows.Forms.TextBox();
+            this.txt_lastname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvNhanVien
+            // 
+            this.lvNhanVien.CheckBoxes = true;
+            this.lvNhanVien.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colLastName,
+            this.colFirstName,
+            this.colPhone});
+            this.lvNhanVien.FullRowSelect = true;
+            this.lvNhanVien.GridLines = true;
+            this.lvNhanVien.HideSelection = false;
+            this.lvNhanVien.Location = new System.Drawing.Point(82, 97);
+            this.lvNhanVien.Name = "lvNhanVien";
+            this.lvNhanVien.Size = new System.Drawing.Size(329, 248);
+            this.lvNhanVien.TabIndex = 1;
+            this.lvNhanVien.UseCompatibleStateImageBehavior = false;
+            this.lvNhanVien.View = System.Windows.Forms.View.Details;
+            this.lvNhanVien.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.lvNhanVien_ColumnClick);
+            this.lvNhanVien.SelectedIndexChanged += new System.EventHandler(this.lvNhanVien_SelectedIndexChanged);
+            // 
+            // colLastName
+            // 
+            this.colLastName.Text = "Last Name";
+            this.colLastName.Width = 75;
+            // 
+            // colFirstName
+            // 
+            this.colFirstName.Text = "First Name";
+            this.colFirstName.Width = 74;
+            // 
+            // colPhone
+            // 
+            this.colPhone.Text = "Phone";
+            this.colPhone.Width = 127;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_addname);
+            this.groupBox1.Controls.Add(this.btn_edit);
+            this.groupBox1.Controls.Add(this.btn_remove);
+            this.groupBox1.Controls.Add(this.txt_phone);
+            this.groupBox1.Controls.Add(this.txt_firstname);
+            this.groupBox1.Controls.Add(this.txt_lastname);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(417, 97);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(303, 248);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin";
+            // 
+            // btn_addname
+            // 
+            this.btn_addname.Location = new System.Drawing.Point(21, 203);
+            this.btn_addname.Name = "btn_addname";
+            this.btn_addname.Size = new System.Drawing.Size(75, 23);
+            this.btn_addname.TabIndex = 9;
+            this.btn_addname.Text = "Add";
+            this.btn_addname.UseVisualStyleBackColor = true;
+            this.btn_addname.Click += new System.EventHandler(this.btn_addname_Click);
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.Location = new System.Drawing.Point(203, 203);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(75, 23);
+            this.btn_edit.TabIndex = 8;
+            this.btn_edit.Text = "Edit";
+            this.btn_edit.UseVisualStyleBackColor = true;
+            this.btn_edit.Click += new System.EventHandler(this.btn_edit_Click);
+            // 
+            // btn_remove
+            // 
+            this.btn_remove.Location = new System.Drawing.Point(101, 203);
+            this.btn_remove.Name = "btn_remove";
+            this.btn_remove.Size = new System.Drawing.Size(97, 23);
+            this.btn_remove.TabIndex = 7;
+            this.btn_remove.Text = "Remove ";
+            this.btn_remove.UseVisualStyleBackColor = true;
+            this.btn_remove.Click += new System.EventHandler(this.btn_remove_Click);
+            // 
+            // txt_phone
+            // 
+            this.txt_phone.Location = new System.Drawing.Point(21, 161);
+            this.txt_phone.Name = "txt_phone";
+            this.txt_phone.Size = new System.Drawing.Size(257, 20);
+            this.txt_phone.TabIndex = 5;
+            this.txt_phone.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txt_firstname
+            // 
+            this.txt_firstname.Location = new System.Drawing.Point(21, 106);
+            this.txt_firstname.Name = "txt_firstname";
+            this.txt_firstname.Size = new System.Drawing.Size(257, 20);
+            this.txt_firstname.TabIndex = 4;
+            this.txt_firstname.TextChanged += new System.EventHandler(this.txt_firstname_TextChanged);
+            // 
+            // txt_lastname
+            // 
+            this.txt_lastname.Location = new System.Drawing.Point(21, 43);
+            this.txt_lastname.Name = "txt_lastname";
+            this.txt_lastname.Size = new System.Drawing.Size(257, 20);
+            this.txt_lastname.TabIndex = 3;
+            this.txt_lastname.TextChanged += new System.EventHandler(this.txt_lastname_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phone";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Last Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lvNhanVien);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvNhanVien;
+        private System.Windows.Forms.ColumnHeader colLastName;
+        private System.Windows.Forms.ColumnHeader colFirstName;
+        private System.Windows.Forms.ColumnHeader colPhone;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_edit;
+        private System.Windows.Forms.Button btn_remove;
+        private System.Windows.Forms.TextBox txt_phone;
+        private System.Windows.Forms.TextBox txt_firstname;
+        private System.Windows.Forms.TextBox txt_lastname;
+        private System.Windows.Forms.Button btn_addname;
+    }
+}
+
---
+++ BTVN_Buoi3/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTVN_Buoi3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lvNhanVien_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lvNhanVien_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+
+        }
+
+
+        private void btn_remove_Click(object sender, EventArgs e)
+        {
+
+
+
+            // Kiểm tra xem có mục nào được chọn trong ListView không
+            if (lvNhanVien.SelectedItems.Count > 0)
+            {
+                // Lặp qua tất cả các mục được chọn và xóa chúng
+                foreach (ListViewItem selectedItem in lvNhanVien.SelectedItems)
+                {
+                    lvNhanVien.Items.Remove(selectedItem); // Xóa mục
+                }
+
+                MessageBox.Show("Đã xóa các mục được chọn.", "Thông báo");
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một hoặc nhiều mục để xóa.", "Thông báo");
+            }
+
+      
+
+
+
+        }
+
+       
+
+        private void btn_addname_Click(object sender, EventArgs e)
+        {
+            ListViewItem lvi = new ListViewItem(txt_lastname.Text);
+            lvi.SubItems.Add(txt_firstname.Text);
+            lvi.SubItems.Add(txt_phone.Text);
+            lvNhanVien.Items.Add(lvi);
+            txt_lastname.Text = "";
+            txt_firstname.Text = "";
+            txt_phone.Text = "";
+
+        }
+
+        private void btn_edit_Click(object sender, EventArgs e)
+        {
+
+          
+            if (lvNhanVien.SelectedItems.Count > 0)
+            {
+         
+                ListViewItem selectedItem = lvNhanVien.SelectedItems[0];
+                selectedItem.Text = txt_lastname.Text; 
+                selectedItem.SubItems[1].Text = txt_firstname.Text; 
+                selectedItem.SubItems[2].Text = txt_phone.Text; 
+
+                MessageBox.Show("Đã cập nhật thông tin thành công!", "Thông báo");
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một mục để chỉnh sửa!", "Thông báo");
+            }
+        }
+
+        private void txt_lastname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void txt_firstname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
